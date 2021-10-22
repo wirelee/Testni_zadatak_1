a94d813a643e6fe666a9e4d8e09e2978ed3430ad
wirelee <50827768+wirelee@users.noreply.github.com>
2021-10-22 15:25:07 +0200

    second commit

---
+++ frmTicker.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Yahoo_Finance_Scraper
+{
+    partial class frmTicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTiker = new System.Windows.Forms.TextBox();
+            this.btnScrape = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.rtbTickerInformation = new System.Windows.Forms.RichTextBox();
+            this.lblTicker = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTiker
+            // 
+            this.txtTiker.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.txtTiker.Location = new System.Drawing.Point(361, 56);
+            this.txtTiker.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTiker.Name = "txtTiker";
+            this.txtTiker.Size = new System.Drawing.Size(128, 26);
+            this.txtTiker.TabIndex = 0;
+            this.txtTiker.Text = "AAPL";
+            // 
+            // btnScrape
+            // 
+            this.btnScrape.Location = new System.Drawing.Point(243, 144);
+            this.btnScrape.Margin = new System.Windows.Forms.Padding(4);
+            this.btnScrape.Name = "btnScrape";
+            this.btnScrape.Size = new System.Drawing.Size(128, 31);
+            this.btnScrape.TabIndex = 1;
+            this.btnScrape.Text = "SCRAPE";
+            this.btnScrape.UseVisualStyleBackColor = true;
+            this.btnScrape.Click += new System.EventHandler(this.btnScraper_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(361, 103);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(128, 26);
+            this.dtpDate.TabIndex = 2;
+            // 
+            // rtbTickerInformation
+            // 
+            this.rtbTickerInformation.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbTickerInformation.Location = new System.Drawing.Point(139, 183);
+            this.rtbTickerInformation.Margin = new System.Windows.Forms.Padding(4);
+            this.rtbTickerInformation.Name = "rtbTickerInformation";
+            this.rtbTickerInformation.Size = new System.Drawing.Size(337, 208);
+            this.rtbTickerInformation.TabIndex = 3;
+            this.rtbTickerInformation.Text = " ";
+            // 
+            // lblTicker
+            // 
+            this.lblTicker.AutoSize = true;
+            this.lblTicker.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTicker.ForeColor = System.Drawing.Color.White;
+            this.lblTicker.Location = new System.Drawing.Point(115, 56);
+            this.lblTicker.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTicker.Name = "lblTicker";
+            this.lblTicker.Size = new System.Drawing.Size(136, 23);
+            this.lblTicker.TabIndex = 4;
+            this.lblTicker.Text = "Choose the Ticker";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(115, 108);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(126, 23);
+            this.lblDate.TabIndex = 5;
+            this.lblDate.Text = "Choose the Date";
+            // 
+            // frmTicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(589, 437);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTicker);
+            this.Controls.Add(this.rtbTickerInformation);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.btnScrape);
+            this.Controls.Add(this.txtTiker);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmTicker";
+            this.Text = "Yahoo! Finance Ticker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTiker;
+        private System.Windows.Forms.Button btnScrape;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.RichTextBox rtbTickerInformation;
+        private System.Windows.Forms.Label lblTicker;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
+
---
+++ frmTicker.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Yahoo_Finance_Scraper
+{
+    public partial class frmTicker : Form
+    {
+        public string _applicationName = "Yahoo! Finance Scraper";
+        public frmTicker()
+        {
+            InitializeComponent();
+            dtpDate.Value = DateTime.Now;
+        }
+
+        private void btnScraper_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTiker.Text))
+            {
+                MessageBox.Show("Ticker not provided.", _applicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                try
+                {
+                    OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\Database\\" + "Database.accdb");
+                    OleDbCommand cmd = con.CreateCommand();
+                    con.Open();
+                    StockInfo stockInfo = null;
+                    //Check if record exists
+                    //If yes update the record
+                    //Pull the record and display on screen
+                    cmd.CommandText = "SELECT * FROM StockInfo WHERE [Ticker] = '" + txtTiker.Text + "' AND PriceDate = #" + dtpDate.Value.ToString("MM-dd-yyyy") + "#";
+                    cmd.Connection = con;
+                    OleDbDataReader oddr = cmd.ExecuteReader();
+                    if (oddr.HasRows)
+                    {
+                        oddr.Read();
+                        stockInfo = new StockInfo(oddr.GetString(1));
+                        stockInfo.CompanyName = oddr.GetString(0);
+                        stockInfo.YearFounded = oddr.GetInt32(2);
+                        stockInfo.NumberOfEmployees = oddr.GetInt32(3);
+                        stockInfo.City = oddr.GetString(4);
+                        stockInfo.State = oddr.GetString(5);
+                        stockInfo.PriceDate = oddr.GetDateTime(6);
+                        stockInfo.PreviousClosePrice = oddr.GetDecimal(7);
+                        stockInfo.OpenPrice = oddr.GetDecimal(8);
+                        stockInfo.MarketCap = oddr.GetString(9);
+                    }
+                    else
+                    {
+                        oddr.Close();
+                        WebClient wc = new WebClient();
+                        stockInfo = wc.RetrieveStock(txtTiker.Text, dtpDate.Value);
+                        cmd.CommandText = "INSERT INTO Stockinfo(CompanyName, Ticker, YearFounded, NumberOfEmployees, City, State, PriceDate, PreviousClosePrice, OpenPrice, MarketCap) VALUES ('"
+                            + stockInfo.CompanyName + "', '" + stockInfo.Ticker + "', '" + stockInfo.YearFounded + "', '" + stockInfo.NumberOfEmployees + "', '" + stockInfo.City + "', '"
+                            + stockInfo.State + "', '" + stockInfo.PriceDate.ToString("dd.MM.yyyy.") + "', '" + stockInfo.PreviousClosePrice + "', '" + stockInfo.OpenPrice + "', '" + stockInfo.MarketCap + "')";
+                        cmd.Connection = con;
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record Submitted", "Congrats");
+                        con.Close();
+                    }
+
+                    if (stockInfo != null)
+                    {
+                        rtbTickerInformation.Text = "";
+                        rtbTickerInformation.Text += "Company Name: " + stockInfo.CompanyName + "\n";
+                        rtbTickerInformation.Text += "Year Founded: " + stockInfo.YearFounded.ToString() + "\n";
+                        rtbTickerInformation.Text += "Number of Employees: " + stockInfo.NumberOfEmployees.ToString() + "\n";
+                        rtbTickerInformation.Text += "City: " + stockInfo.City + "\n";
+                        rtbTickerInformation.Text += "State: " + stockInfo.State + "\n";
+                        rtbTickerInformation.Text += "Price Date Time: " + stockInfo.PriceDate.ToShortDateString() + "\n";
+                        rtbTickerInformation.Text += "Previous Close Price: " + stockInfo.PreviousClosePrice.ToString() + "\n";
+                        rtbTickerInformation.Text += "Open Price: " + stockInfo.OpenPrice.ToString() + "\n";
+                        rtbTickerInformation.Text += "Market Cap: " + stockInfo.MarketCap + "\n";
+                        
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    //Write error handling
+                    MessageBox.Show("Error info: " + ex.Message);
+
+                }
+            }
+        }
+    }
+}
+
+
